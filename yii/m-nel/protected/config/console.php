878cff90bb3a4ecf70ef6fc23e92069dd3a46a05
Michael Nel <michael.a.nel1@gmail.com>
2013-11-20 15:41:38 +0200

    Updated console config & removed auto generated sql files

--- yii/m-nel/protected/config/console.php
+++ yii/m-nel/protected/config/console.php
@@ -4,7 +4,7 @@
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
+	'name'=>'Yii - Todo PHP',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -12,18 +12,8 @@ return array(
 	// application components
 	'components'=>array(
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/yiitodophp.db',
 		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
