6df03e9a992ae2f94146c183fb7bb5ea6122435b
Michael Nel <michael.a.nel1@gmail.com>
2013-11-21 18:03:14 +0200

    updated test config to disable UrlManager - otherwise URL creation disregards index-test.php resulting in not using test config

--- yii/m-nel/protected/config/test.php
+++ yii/m-nel/protected/config/test.php
@@ -4,6 +4,7 @@ return CMap::mergeArray(
 	require(dirname(__FILE__).'/main.php'),
 	array(
 		'components'=>array(
+            'urlManager'=>null,
 			'fixture'=>array(
 				'class'=>'system.test.CDbFixtureManager',
 			),
