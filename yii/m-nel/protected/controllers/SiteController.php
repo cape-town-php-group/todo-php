465fb3abdad6e6406594fc523ebeaed1c531d8ec
Michael Nel <michael.a.nel1@gmail.com>
2013-11-21 10:50:19 +0200

    Updated error handler

--- yii/m-nel/protected/controllers/SiteController.php
+++ yii/m-nel/protected/controllers/SiteController.php
@@ -2,29 +2,15 @@
 
 class SiteController extends Controller
 {
-
 	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
-	public function actionIndex()
-	{
-		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
-	}
-
-	/**
-	 * This is the action to handle external exceptions.
+	 * This is the action to handle errors/exceptions.
 	 */
 	public function actionError()
 	{
-		if($error=Yii::app()->errorHandler->error)
+        $error = Yii::app()->errorHandler->error;
+		if($error)
 		{
-			if(Yii::app()->request->isAjaxRequest)
-				echo $error['message'];
-			else
-				$this->render('error', $error);
+			$this->render('error', $error);
 		}
 	}
 }
--- yii/m-nel/protected/views/site/error.php
+++ yii/m-nel/protected/views/site/error.php
@@ -1,15 +1,11 @@
 <?php
 /* @var $this SiteController */
 /* @var $error array */
-
-$this->pageTitle=Yii::app()->name . ' - Error';
-$this->breadcrumbs=array(
-	'Error',
-);
 ?>
 
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-<?php echo CHtml::encode($message); ?>
-</div>
+<header id="header">
+    <h1>error <?php echo $code; ?></h1>
+    <div class="error">
+        <?php echo CHtml::encode($message); ?>
+    </div>
+</header>
