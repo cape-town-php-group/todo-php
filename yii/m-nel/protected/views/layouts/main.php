0b0f384e1fba8769a29caa08775422c8abad9769
Michael Nel <michael.a.nel1@gmail.com>
2013-11-21 10:51:13 +0200

    Fixed incorrect reference to todoMVC

--- yii/m-nel/protected/views/layouts/main.php
+++ yii/m-nel/protected/views/layouts/main.php
@@ -22,7 +22,7 @@
         
         <!-- Change this out with your name and url ↓ -->
         <p>Created by <a href="https://github.com/m-nel">Michael Nel</a></p>
-        <p>Part of <a href="https://github.com/cape-town-php-group/todo-php">TodoMVC</a></p>
+        <p>Part of <a href="https://github.com/cape-town-php-group/todo-php">TodoPHP</a></p>
     </footer>
 
 </body>
