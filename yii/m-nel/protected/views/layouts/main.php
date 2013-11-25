e835a901d12ad772fd684e805a939e08c2ac2fa6
Michael Nel <michael.a.nel1@gmail.com>
2013-11-25 11:49:58 +0200

    Fixed bad resource reference

--- yii/m-nel/protected/views/layouts/main.php
+++ yii/m-nel/protected/views/layouts/main.php
@@ -6,9 +6,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Yii - TodoPHP</title>
-    <link rel="stylesheet" href="/css/base.css">
+    <link rel="stylesheet" href="<?php echo Yii::app()->baseUrl; ?>/css/base.css">
     <!-- CSS overrides -->
-    <link rel="stylesheet" href="/css/todo.css">
+    <link rel="stylesheet" href="<?php echo Yii::app()->baseUrl; ?>/css/todo.css">
 </head>
 <body>
     
@@ -17,10 +17,6 @@
     </section>
     
     <footer id="info">
-        <!-- Don't know if this functionality is required -->
-        <!--<p>Double-click to edit a todo</p>-->
-        
-        <!-- Change this out with your name and url ↓ -->
         <p>Created by <a href="https://github.com/m-nel">Michael Nel</a></p>
         <p>Part of <a href="https://github.com/cape-town-php-group/todo-php">TodoPHP</a></p>
     </footer>
